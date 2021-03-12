3bb4337069928ab4aa213f161b54600ba1bbf481
ybstn <67552572+ybstn@users.noreply.github.com>
2021-03-13 02:22:37 +0300

    Delete Yulya directory

--- Yulya/Models/PortfolioGalleryContent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Yulya.Models
-{
-    public class PortfolioGalleryContent
-    {
-        public string ImgUrl { get; set; }
-        public int index { get; set; }
-    }
-}
